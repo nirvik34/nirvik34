2728e1f3c443dd78eba25ccadaa4921cb4676835
nirvik34 <jarhnirvik@gmail.com>
2026-01-14 23:40:08 +0530

    Adding details

--- today.py
+++ today.py
@@ -322,18 +322,18 @@ def svg_overwrite(filename, age_data, commit_data, star_data, repo_data, contrib
     """
     tree = etree.parse(filename)
     root = tree.getroot()
-    justify_format(root, 'commit_data', commit_data, 22)
-    justify_format(root, 'star_data', star_data, 14)
-    justify_format(root, 'repo_data', repo_data, 6)
+    justify_format(root, 'commit_data', commit_data)
+    justify_format(root, 'star_data', star_data)
+    justify_format(root, 'repo_data', repo_data)
     justify_format(root, 'contrib_data', contrib_data)
-    justify_format(root, 'follower_data', follower_data, 10)
-    justify_format(root, 'loc_data', loc_data[2], 9)
+    justify_format(root, 'follower_data', follower_data)
+    justify_format(root, 'loc_data', loc_data[2])
     justify_format(root, 'loc_add', loc_data[0])
-    justify_format(root, 'loc_del', loc_data[1], 7)
+    justify_format(root, 'loc_del', loc_data[1])
     tree.write(filename, encoding='utf-8', xml_declaration=True)
 
 
-def justify_format(root, element_id, new_text, length=0):
+def justify_format(root, element_id, new_text):
     """
     Updates and formats the text of the element, and modifes the amount of dots in the previous element to justify the new text on the svg
     """
@@ -341,13 +341,7 @@ def justify_format(root, element_id, new_text, length=0):
         new_text = f"{'{:,}'.format(new_text)}"
     new_text = str(new_text)
     find_and_replace(root, element_id, new_text)
-    just_len = max(0, length - len(new_text))
-    if just_len <= 2:
-        dot_map = {0: '', 1: ' ', 2: '. '}
-        dot_string = dot_map[just_len]
-    else:
-        dot_string = ' ' + ('.' * just_len) + ' '
-    find_and_replace(root, f"{element_id}_dots", dot_string)
+    find_and_replace(root, element_id, new_text)
 
 
 def find_and_replace(root, element_id, new_text):
